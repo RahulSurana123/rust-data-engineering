94eb459f3fa11448d0b94178590a50f234918f35
suru_4851 <35515322+RahulSurana123@users.noreply.github.com>
2025-02-06 18:40:54 +0000

    graph challenge solved

--- lisbon-shortest-path/src/main.rs
+++ lisbon-shortest-path/src/main.rs
@@ -1,5 +1,13 @@
 use petgraph::algo::dijkstra;
 use petgraph::prelude::*;
+use petgraph::visit::NodeRef;
+use std::io::stdin;
+
+fn read() -> usize {
+    let mut val = String::new();
+    stdin().read_line(&mut val);
+    val.trim().parse().unwrap()
+}
 
 fn main() {
     let mut graph = Graph::<&str, u32, Undirected>::new_undirected();
@@ -18,16 +26,37 @@ fn main() {
         (monastery, commerce_square, 6), // The distance from Jerónimos Monastery to Commerce Square is 6 km
         (lx_factory, commerce_square, 5), // The distance from LX Factory to Commerce Square is 5 km
         (commerce_square, lisbon_cathedral, 1), // The distance from Commerce Square to Lisbon Cathedral is 1 km
+        (lx_factory, commerce_square, 10), // new edge from LX Factory to Commerce Square is 10 km
     ]);
 
-    let node_map = dijkstra(&graph, belem_tower, Some(lisbon_cathedral), |e| *e.weight());
+    for (i, node) in graph.node_weights().into_iter().enumerate() {
+        println!(" {i}. {node} ",);
+    }
+    
+    println!("Please Select source Node number : ");
+    let src = read();
+    println!("Please Select destination Node number : ");
+    let dest = read();
 
-    if let Some(distance) = node_map.get(&lisbon_cathedral) {
+    let src_name = graph.node_weights().into_iter().nth(src);
+    let dest_name = graph.node_weights().into_iter().nth(dest);
+    let src = graph.node_indices().into_iter().nth(src).unwrap();
+    let dest = graph.node_indices().into_iter().nth(dest);
+
+    let node_map = dijkstra(&graph, src, dest, |e| *e.weight());
+
+    if let Some(distance) = node_map.get(&dest.unwrap()) {
         println!(
-            "The shortest distance from Belem Tower to Lisbon Cathedral is {} km",
+            "The shortest distance from {:?} to {:?} is {} km",
+            src_name.unwrap(),
+            dest_name.unwrap(),
             distance
         );
     } else {
-        println!("No route found from Belem Tower to Lisbon Cathedral.");
+        println!(
+            "No route found from {:?} to {:?}.",
+            src_name.unwrap(),
+            dest_name.unwrap()
+        );
     }
 }
