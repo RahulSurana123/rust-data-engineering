386867afc3bcbb4339792fe098bf89c1d9c68d5f
suru_4851 <35515322+RahulSurana123@users.noreply.github.com>
2025-02-06 19:31:35 +0000

    graph visualize Challenge done

--- graph-visualize/src/main.rs
+++ graph-visualize/src/main.rs
@@ -1,20 +1,30 @@
 extern crate rasciigraph;
 
-use rasciigraph::{plot, Config};
+use rasciigraph::{plot, plot_many, Config};
 
 fn main() {
-    let cities = vec!["Lisbon", "Madrid", "Paris", "Berlin", "Copenhagen", "Stockholm", "Moscow"];
+    let cities = vec![
+        "Lisbon",
+        "Madrid",
+        "Paris",
+        "Berlin",
+        "Copenhagen",
+        "Stockholm",
+        "Moscow",
+    ];
     let distances_travelled = vec![0.0, 502.56, 1053.36, 2187.27, 2636.42, 3117.23, 4606.35];
 
+    let distances_travelled = vec![0.0, 150.56, 1103.36, 2217.27, 2266.42, 3000.0];
     println!("{}", cities.join(" > "));
 
     println!(
         "{}",
-        plot(
-            distances_travelled.into_iter().map(|d| d as f64).collect(),
+        plot_many(
+            vec![distances_travelled.clone(), distances_travelled],
             Config::default()
                 .with_offset(10)
                 .with_height(10)
+                .with_width(15)
                 .with_caption("Travelled distances (km)".to_string())
         )
     );
