fd4c6953e8ce6b9928b4f57edb65b33493c2a462
Rahul Surana <rasuru04@gmail.com>
2025-02-25 04:27:52 +0000

    adding challenges code

--- mutable-fruit-salad/src/main.rs
+++ mutable-fruit-salad/src/main.rs
@@ -6,6 +6,18 @@ fn main() {
     // Uncommenting the following line will cause a compilation error because fruit_salad 
     // is immutable.
     // fruit_salad.push("figs");
+    let mut fruit_salad = vec!["apple", "banana", "dates", "cherry", "elderberries"];
+    println!("Original fruit salad: {:?}", fruit_salad);
+
+    // Uncommenting the following line will cause a compilation error because fruit_salad is immutable.
+    fruit_salad.push("figs");
+    fruit_salad.pop();
+    fruit_salad.retain(|&x| x != "apple");
+    fruit_salad.sort();
+
+    for i in fruit_salad{
+        println!("{i}");
+    }
 
     // To mutate the vector, we need to declare it as mutable:
     let mut fruit_salad = vec!["apple", "banana", "cherry", "dates", "elderberries"];
